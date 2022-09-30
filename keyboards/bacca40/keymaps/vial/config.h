88beeab703642b96016fd16b483db451afafc1d3
Ele Thompson <ele.thompson12@gmail.com>
2022-09-30 01:14:53 -0700

    Added bacca40 VIAL support

---
+++ keyboards/bacca40/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x72, 0x84, 0xF9, 0x1E, 0x17, 0x94, 0xC6, 0x76}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
