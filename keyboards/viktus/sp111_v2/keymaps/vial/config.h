41a47c5708ed2adb952528f860f28b7373a04a0b
Ele Thompson <ele.thompson12@gmail.com>
2023-02-25 12:03:48 -0800

    [Keyboard] Added SP111 v2 firmware

---
+++ keyboards/viktus/sp111_v2/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x24, 0x31, 0xD5, 0xAB, 0xAC, 0x47, 0x0D, 0xFB}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 9, 7 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
